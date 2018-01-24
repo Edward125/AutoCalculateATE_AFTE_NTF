25a5d2aeff462e2abae54847cca220b754cdc5de
Edward <edward_song@yeah.net>
2018-01-24 17:00:28 +0800

    create

---
+++ LearningSystem/LearningSystem/frmMain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LearningSystem
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            skinEngine1.SkinFile = p.AppFolder + @"\MacOS.ssk";
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            loadUI();
+        }
+
+        private void loadUI()
+        {
+            // skinEngine1.SkinFile = p.AppFolder + @"\MacOS.ssk";
+            this.Text = "Compare FTP files & DB Files,Ver:" + Application.ProductVersion + "(Edward_song@yeah.net)";
+        }
+
+    }
+}
